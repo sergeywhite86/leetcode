3b5b2306a30e90ad4b8c6e5c1ce25bceeef30ee1
Sergey White <sergey_white86@mail.ru>
2025-03-04 12:02:02 +0300

    two sum

---
+++ src/main/java/org/sergey_white/two_sum/Solution.java
@@ -0,0 +1,55 @@
+package org.sergey_white.two_sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * Example 2:
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * Example 3:
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ **/
+class Solution {
+
+    /**
+     * O(n)2
+     */
+    public static int[] twoSumOn2(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
+                if (sum == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * O(n)
+     */
+    public static int[] twoSumON(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[]{numToIndex.get(complement), i};
+            }
+            numToIndex.put(nums[i], i);
+        }
+        return null;
+    }
+}
